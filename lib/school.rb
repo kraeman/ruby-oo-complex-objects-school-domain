a8131f5b7cfabd156a1ec12668a3c4d7412d95b6
krae <kraewind@gmail.com>
2020-11-03 11:16:21 -0500

    commit tree c18c3290c6da47eaebffa69c66e5a98ad3639da7

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    
+    end
+
+
+    def roster
+        @roster
+    end
+
+    def add_student(student, grade)
+        @student = student.split(", ")
+        @grade = grade
+        while @roster[@grade] == nil
+            @roster[@grade] = []
+        end
+        @student.map do |person|
+            @roster[@grade].push(person)
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.map do |key, value|
+            @roster[key] = value.sort
+        end
+        @roster
+    end
+end
